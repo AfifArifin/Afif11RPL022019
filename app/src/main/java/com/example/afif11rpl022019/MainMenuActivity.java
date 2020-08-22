83c6a090d96f96ee03058b149c524bd76405d645
Afif_Arifin <afif.10rpl2.2019@gmail.com>
2020-08-22 13:26:31 +0700

    add splash screen

--- app/src/main/java/com/example/afif11rpl022019/MainMenuActivity.java
+++ app/src/main/java/com/example/afif11rpl022019/MainMenuActivity.java
@@ -2,17 +2,24 @@ package com.example.afif11rpl022019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    Button btnAction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        btnAction = findViewById(R.id.btnAction);
+
 
     }
 }
